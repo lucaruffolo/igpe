f9290a4155141670c6358444d3924dceed61bff8
Luca <Luca@DESKTOP-S7J2LDG.homenet.telecomitalia.it>
2020-05-01 20:26:51 +0200

    Fixxato problema sara

--- src/project/igpe/GUI/Credits.java
+++ src/project/igpe/GUI/Credits.java
@@ -1,11 +1,10 @@
 package project.igpe.GUI;
 
-import java.awt.Button;
-import java.awt.event.ActionEvent;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import project.igpe.main.Main;
