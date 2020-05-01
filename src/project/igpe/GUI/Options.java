2b96284b5624a6b786927b331e75782dd5e5c003
topie <topie@DESKTOP-0QVUTCI.station>
2020-05-01 16:42:08 +0200

    crazione classe opzioni

---
+++ src/project/igpe/GUI/Options.java
@@ -0,0 +1,62 @@
+package project.igpe.GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+
+public class Options {
+
+    @FXML
+    private Slider sliderMusic;
+
+    @FXML
+    private CheckBox bttFullscreen;
+
+    @FXML
+    private Label lblEffects;
+
+    @FXML
+    private Slider sliderEffects;
+
+    @FXML
+    private AnchorPane MenuOptions;
+
+    @FXML
+    private Button bttApply;
+
+    @FXML
+    private Label lblMusic;
+
+    @FXML
+    private Button bttBack;
+
+    @FXML
+    void DragEffects(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DragMusic(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ClickFullscreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ClickBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ClickApply(ActionEvent event) {
+
+    }
+
+}
