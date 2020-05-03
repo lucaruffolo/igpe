dc90cb84741e2c2a5401e77728568bfd2c3e82aa
Luca <Luca@DESKTOP-S7J2LDG.homenet.telecomitalia.it>
2020-05-03 16:49:24 +0200

    sdp 2.0

--- src/project/igpe/GUI/SceltaDelPersonaggio.java
+++ src/project/igpe/GUI/SceltaDelPersonaggio.java
@@ -1,7 +1,5 @@
 package project.igpe.GUI;
 
-import java.io.File;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import project.igpe.classes.GraphicHero;
@@ -20,11 +17,6 @@ import project.igpe.classes.Movement;
 import project.igpe.main.Main;
 
 public class SceltaDelPersonaggio {
-	
-	private Image imageM = new Image(GraphicHero.class.getResourceAsStream(".."+File.separator+"images" + File.separator + "maschio.png"));
-	private Image imageF = new Image(GraphicHero.class.getResourceAsStream(".."+File.separator+"images" + File.separator + "femmina.png"));
-	private ImageView imageViewM = new ImageView(imageM);
-	private ImageView imageViewF = new ImageView(imageF);
 	
 	private static Boolean sesso=false;
     @FXML
@@ -49,10 +41,10 @@ public class SceltaDelPersonaggio {
     private Button bttBack;
     
     @FXML
-    private ImageView imgFemmina = new ImageView(imageF);
+    private ImageView imgFemmina;
     
     @FXML
-    private ImageView imgMaschio = new ImageView(imageM);
+    private ImageView imgMaschio;
 
     
     @FXML
