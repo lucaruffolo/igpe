61ac09cc4cc410a140d06a01dca5cef5ab11fade
topie <topie@DESKTOP-0QVUTCI.station>
2020-05-04 18:40:23 +0200

    we

--- src/project/igpe/GUI/Sound.java
+++ src/project/igpe/GUI/Sound.java
@@ -9,14 +9,5 @@ import javax.sound.sampled.Clip;
 public class Sound {
 
 	public void playSound() {
-	    try {
-	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/project/igpe/sounds/CABRIOLET.mp3").getAbsoluteFile());
-	        Clip clip = AudioSystem.getClip();
-	        clip.open(audioInputStream);
-	        clip.start();
-	    } catch(Exception ex) {
-	        System.out.println("Error with playing sound.");
-	        ex.printStackTrace();
-	    }
-	}
+	 
 }
