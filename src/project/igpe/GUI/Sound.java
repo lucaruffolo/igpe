8c7e845ab614f260f8b7ad7891c34ffddd9e2673
topie <topie@DESKTOP-0QVUTCI.station>
2020-05-04 20:01:56 +0200

    il suono funzione...cibbo fa cagà

--- src/project/igpe/GUI/Sound.java
+++ src/project/igpe/GUI/Sound.java
@@ -1,5 +1,34 @@
 package project.igpe.GUI;
 
+import java.io.File;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 public class Sound {
-//ora risolvo
+	
+	
+	private static Media gamesound;
+	private static MediaPlayer mediaPlayer;
+	
+	public static void setMusic(String musicFile) {
+		gamesound = new Media(new File(musicFile).toURI().toString());
+		mediaPlayer = new MediaPlayer(gamesound);
+	}
+	
+	public static void modifyVolume(double x) {
+		mediaPlayer.setVolume(x);
+	}
+	
+	public static void musicStart() {
+			mediaPlayer.play();
+	}
+	
+	public static void musicStop() {
+		mediaPlayer.stop();
+	}
+
+	
+	
+	
 }
