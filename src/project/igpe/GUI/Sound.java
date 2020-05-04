3b181fa58a9d81145f65d380489d297dc547dbc9
topie <topie@DESKTOP-0QVUTCI.station>
2020-05-04 18:40:47 +0200

    we2

--- src/project/igpe/GUI/Sound.java
+++ src/project/igpe/GUI/Sound.java
@@ -1,13 +1,5 @@
 package project.igpe.GUI;
 
-import java.io.File;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-
 public class Sound {
 
-	public void playSound() {
-	 
 }
