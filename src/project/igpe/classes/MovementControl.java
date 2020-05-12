e6703568be00dfa6103a28c277bcc5e96b718040
Luca <Luca@DESKTOP-S7J2LDG.homenet.telecomitalia.it>
2020-05-12 15:44:45 +0200

    swag

--- src/project/igpe/classes/MovementControl.java
+++ src/project/igpe/classes/MovementControl.java
@@ -1,15 +1,14 @@
 package project.igpe.classes;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import project.igpe.GUI.MenuIniziale;
+import project.igpe.GUI.ChangeRoomScene;
 import project.igpe.main.Main;
 
 
@@ -43,6 +42,9 @@ public class MovementControl implements EventHandler<KeyEvent> {
 			movimento.move(Movement.MOVE_DOWN);
 			graphics.draw();
 			break;
+		case X:
+			Hero.setLife(100);			
+			graphics.draw();
 		case L:
 			if (Hero.getLife()>0)
 				Hero.setLife(Hero.getLife()-10);			
@@ -51,48 +53,37 @@ public class MovementControl implements EventHandler<KeyEvent> {
 			
 		case ESCAPE:
 			
-			if (graphics.getScene().getWindow() instanceof Stage) {
+			//f (graphics.getScene().getWindow() instanceof Stage) {
 			
 				sceneGame = Main.window.getScene();
-				FXMLLoader loaderSleep = new FXMLLoader(MenuIniziale.class.getResource("SleepScene.fxml"));
+				
+				FXMLLoader loaderSleep = new FXMLLoader(ChangeRoomScene.class.getResource("ChangeRoomScene.fxml"));
 				AnchorPane rootSleep = null;
 				try {rootSleep = (AnchorPane) loaderSleep.load();} catch (IOException e2) {e2.printStackTrace();	}
-				Scene menuSleep = new Scene(rootSleep, 1270, 900);
-				menuSleep.setFill(Color.BLACK);
-				
-				
-			//	Main.window.setScene(menuSleep);
+				Scene menuSleep = new Scene(rootSleep, 1270, 900);	
+				Main.window.setScene(menuSleep);
 				Main.window.centerOnScreen();
+				try {	TimeUnit.SECONDS.sleep(1);	} catch (InterruptedException e2) {	e2.printStackTrace();	}
+
+				//try {	TimeUnit.MICROSECONDS.sleep(10);	} catch (InterruptedException e2) {	e2.printStackTrace();}
+				
+		
 				System.out.println("wait");
 			
-				
+				/*
 				// Switcho sul menu di PAUSA
 				FXMLLoader loader = new FXMLLoader(MenuIniziale.class.getResource("Pausa.fxml"));  //prendiamo il file dalla classe che è legata all'interfaccia
 				AnchorPane root = null;
 				try { root = (AnchorPane) loader.load(); } catch (IOException e1) {	e1.printStackTrace();}			
 				Scene menuPausa = new Scene(root, 1270, 900); 
+				try {	TimeUnit.SECONDS.sleep(1);	} catch (InterruptedException e2) {	e2.printStackTrace();	}
 				Main.window.setScene(menuPausa);
 				Main.window.centerOnScreen();
-				
-			}
+				*/
+			
 			break;
 		case P:
-			
-	//		Stage stage = (Stage) graphics.getScene().getWindow();
-			System.out.println("Wait in corso");
-	//		stage.showAndWait();
-			
-			// Switcho sul menu di PAUSA
-			FXMLLoader loader = new FXMLLoader(MenuIniziale.class.getResource("Pausa.fxml"));  //prendiamo il file dalla classe che è legata all'interfaccia
-			AnchorPane root = null;
-			try { root = (AnchorPane) loader.load(); } catch (IOException e1) {	e1.printStackTrace();}			
-			Scene menuPausa = new Scene(root, 1024,720); 
-			Main.window.setScene(menuPausa);
-			Main.window.centerOnScreen();
-
-			
-			
-			
+				
 			break;
 		default:
 			break;
