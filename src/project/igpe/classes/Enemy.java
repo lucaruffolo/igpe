d5b7e669e3eda9183bbb40e3bb2e8d2dc65ab328
Luca <Luca@DESKTOP-S7J2LDG.homenet.telecomitalia.it>
2020-09-01 22:12:39 +0200

    Fix Movement enemy

--- src/project/igpe/classes/Enemy.java
+++ src/project/igpe/classes/Enemy.java
@@ -3,6 +3,9 @@ package project.igpe.classes;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
 public class Enemy {
 	
 	public final static int MOVE_RIGHT = 0;
@@ -30,58 +33,94 @@ public class Enemy {
 		life = 200;
 		contenitoreBullets = new ArrayList<Bullet>();
 	} 
-	public static int nRand = (int) (4.0 * Math.random());
+	
+	public static int nRandDir = (int) (4.0 * Math.random());
 	
 	public static void moveEnemy() {
 
-	    moving(nRand);
-		
+	    moving(nRandDir);
 		/*		
 		if (Movement.room.getCellType(Movement.pixelInMatrixX(x), Movement.pixelInMatrixX(y)) == Cell.EMPTY) {
 			Movement.room.setCellType(Movement.pixelInMatrixX(x), Movement.pixelInMatrixX(y), Cell.ENEMY);
 		}
 		*/
 	}
-	
 
-	public static void moving (int dir) {
-		if (!Movement.collisionWall(x, y) && !Movement.collisionDoor(x, y) && !Movement.collisionObstacle(x, y) && !Movement.collisionHero(x, y)){
+	public static int contadir = 0;
 	
-			if (dir == MOVE_RIGHT) {
-				x += speed;
-			}
-			if (dir == MOVE_LEFT) {
-				x -= speed;		
-			}
-			if (dir == MOVE_UP) {
-				y -= speed;
-			}
-			if (dir == MOVE_DOWN) {
+	public static boolean stopMoving = false;
+	public static void moving (int dir) {
+		
+		dirEnemy = dir;
+		if (!Movement.collisionWall(x, y) && !Movement.collisionDoor(x, y) && !Movement.collisionObstacle(x, y) && !Movement.collisionHero(x, y)){
+
+			if (dir == MOVE_RIGHT && !stopMoving)
+				x += speed;		
+			if (dir == MOVE_LEFT && !stopMoving)
+				x -= speed;				
+			if (dir == MOVE_UP && !stopMoving)
+				y -= speed;		
+			if (dir == MOVE_DOWN && !stopMoving) 
+				y += speed;	
+			if(x==Settings.x/4 || y==Settings.y/4 || x==Settings.x/2 || y==Settings.y/2) {
 				y += speed;
-			}
-		}
-		else {
-			if (dir == MOVE_RIGHT) {
-				x -= speed;
-			}
-			if (dir == MOVE_LEFT) {
 				x += speed;
+				service.restart();
 			}
-			if (dir == MOVE_UP) {
-				y += speed;
+		} else {			
+			
+			int direzione = nRandDir;		
+			while(nRandDir == direzione) {
+				nRandDir = (int) (4.0 * Math.random());				
 			}
-			if (dir == MOVE_DOWN) {
-				y -= speed;
+			if (dir == MOVE_RIGHT) 
+				x -= speed*3;			
+			if (dir == MOVE_LEFT) 
+				x += speed*3;			
+			if (dir == MOVE_UP) 
+				y += speed*3;			
+			if (dir == MOVE_DOWN) 
+				y -= speed*3;
+		
+			service.restart();
+			contadir++;
+			System.out.println("dir "+ nRandDir+" cambio direzione ->" + contadir);
+				
 				
 			}
-			int direzione = nRand;		
-			while(nRand == direzione){				
-				System.out.println(nRand);
-				nRand = (int) (4.0 * Math.random());
-			}			
-						
-		}
+		
 	}
+
+
+
+	static Service<Void> service = new Service<Void>() { //like timer
+		@Override
+		protected Task<Void> createTask() {
+			return new Task<Void>() {				
+				@Override
+				protected Void call() throws Exception {
+
+					int value = 0;					
+					while(value <= 15) {
+						
+						value++;
+						stopMoving=true;
+						Thread.sleep(35);					
+					}
+					
+					int direzione = nRandDir;		
+					while(nRandDir == direzione) {
+						nRandDir = (int) (4.0 * Math.random());				
+					}
+					
+					stopMoving = false;	
+					
+				return null;
+				} 
+			};
+		}
+	};
+
 	
 	public static List<Bullet> getContenitoreBullets() {
 		return contenitoreBullets;
